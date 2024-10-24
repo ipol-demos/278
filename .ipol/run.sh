e0c18b5cf052aa47c87c2140e399ce1a8099f721
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2024-10-24 15:13:45 +0530

    move input_ files to current execution directory

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -70,7 +70,7 @@ for FILE in ${FLA[@]}; do
       PNGFILE="${UNPACKED}/input_${FILENUM}.png" # new .png file name 
       convert "${FILENEW}" "${PNGFILE}" # convert to .png 
       # After successful conversion, move .png file to current folder 
-      mv -v "${PNGFILE}""./input_${FILENUM}.png" 
+      mv -v "${PNGFILE}" "./input_${FILENUM}.png" 
       FLAMOD[${FILENUM}]="./input_${FILENUM}.png" # update array with .png file in current folder 
       rm -v "${FILENEW}" # remove original .jpg file after conversion (optional) 
       FLAMOD[${FILENUM}]="./input_${FILENUM}.png" # update array with .png file
