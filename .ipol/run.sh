6b260f6dfc9a0cf75d35512d3865460ab30d1a44
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2024-10-24 22:50:31 +0530

    located executables

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -129,8 +129,8 @@ FLAMOD=( "${FLAMOD[@]/#/$CURP/}" )
 echo $FLAMOD
 
 CMD1=$(echo "(octave -W -qf $bin/run_ef.m $PARAM_EF ""${FLAMOD[@]})")
-CMD2=$(echo "(octave -W -qf $bin/runeef.m $PARAMEEF ""${FLAMOD[@]})")
-parallel ::: "$CMD1" "$CMD2"
+#CMD2=$(echo "(octave -W -qf $bin/runeef.m $PARAMEEF ""${FLAMOD[@]})")
+#parallel ::: "$CMD1" "$CMD2"
 #mv /workdir/exec/*.png /workdir/exec/algo_info.txt .  # recup the generated files
 TIMEFUSION=$(($(date +%s) - $TIME))
 
