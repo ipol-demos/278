0cf58c14cb03eb03bc8bca0000e1c59153d839d8
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2025-03-06 22:21:57 +0530

    located binaries

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -126,8 +126,8 @@ CURP=$(pwd)
 ### prepend $IMGP to all images (in $FLA)
 # FLA=( "${FLA[@]/#/$IMGP/}" )
 FLAMOD=( "${FLAMOD[@]/#/$CURP/}" )
-CMD1=$(echo "(octave -W -qf run_ef.m $PARAM_EF ""${FLAMOD[@]})")
-CMD2=$(echo "(octave -W -qf runeef.m $PARAMEEF ""${FLAMOD[@]})")
+CMD1=$(echo "(octave -W -qf $bin/run_ef.m $PARAM_EF ""${FLAMOD[@]})")
+CMD2=$(echo "(octave -W -qf $bin/runeef.m $PARAMEEF ""${FLAMOD[@]})")
 parallel ::: "$CMD1" "$CMD2"
 mv ${BIN}/*.png ${BIN}/algo_info.txt .  # recup the generated files
 TIMEFUSION=$(($(date +%s) - $TIME))
