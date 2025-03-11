6b3f6e4909c14370e561b1c470e584f54f2af44a
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2025-03-11 19:32:52 +0530

    check if image readable

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -142,7 +142,9 @@ echo "$INFO"
 CMD1=$(octave -W -qf ${BIN}/run_ef.m $PARAM_EF "${FLAMOD[@]}")
 #CMD2=$(cd ${BIN} && octave -W -qf runeef.m $PARAMEEF "${FLAMOD[@]}")
 
-mv ${BIN}/run_ef.m ${BIN}/
+mv ${BIN}/run_ef.m ${BIN}/ .
+mv ${BIN}/exposureFusion ${BIN}/ .
+mv ${BIN}/robustNormalization.m  ${BIN}/ .
 octave -W -qf run_ef.m 1 1 0 input_0.png input_1.png input_2.png input_3.png
 
 #parallel ::: "$CMD1" "$CMD2"
