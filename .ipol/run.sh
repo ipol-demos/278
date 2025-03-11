6f8fab80d417b12f6f8eca4a66f6375eac49e8ca
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2025-03-11 18:42:20 +0530

    check if image readable

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -140,11 +140,11 @@ INFO=$(identify -format "Format: %m/nDimensions: %w×%h/nSize: %b/n" input_0.png
 echo "$INFO"
 
 CMD1=$(octave -W -qf ${BIN}/run_ef.m $PARAM_EF "${FLAMOD[@]}")
-CMD2=$(cd ${BIN} && octave -W -qf runeef.m $PARAMEEF "${FLAMOD[@]}")
+#CMD2=$(cd ${BIN} && octave -W -qf runeef.m $PARAMEEF "${FLAMOD[@]}")
 
-cd ${BIN} && octave -W -qf run_ef.m 1 1 0 "${FLAMOD[@]}"
+cd ${BIN} && octave -W -qf run_ef.m 1 1 0 input_0.png input_1.png input_2.png input_3.png
 
-parallel ::: "$CMD1" "$CMD2"
+#parallel ::: "$CMD1" "$CMD2"
 mv ${BIN}/*.png ${BIN}/ .  # recup the generated files
 TIMEFUSION=$(($(date +%s) - $TIME))
 
