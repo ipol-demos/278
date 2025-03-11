df12ddea42394acfd874e902d5561fefac65e538
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2025-03-11 12:37:39 +0530

    keeping the original CMD1

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -125,9 +125,10 @@ CURP=$(pwd)
 FLAMOD=( "${FLAMOD[@]/#/$CURP/}" )
 echo "it's reaching this point"
 
-octave -W -qf /workdir/bin/run_ef.m 1 1 0 "${FLAMOD[@]}"
+#octave -W -qf /workdir/bin/run_ef.m 1 1 0 "${FLAMOD[@]}"
 
-#CMD1=$(echo "(cd /workdir/bin && octave -W -qf run_ef.m $PARAM_EF ""${FLAMOD[@]})")
+CMD1=$(echo "(octave -W -qf /workdir/bin/run_ef.m $PARAM_EF ""${FLAMOD[@]})")
+
 #CMD2=$(echo "(cd /workdir/bin && octave -W -qf runeef.m $PARAMEEF ""${FLAMOD[@]})")
 #parallel ::: "$CMD1" "$CMD2"
 #mv ${BIN}/*.png ${BIN}/ .  # recup the generated files
