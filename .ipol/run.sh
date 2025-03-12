a4c6fba4ac0db8afe37998b38fd666109924fb04
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2025-03-12 22:38:07 +0530

    add code to run.sh

--- .ipol/run.sh
+++ .ipol/run.sh
@@ -145,7 +145,7 @@ echo "$INFO"
 #cp ${BIN}/run_ef.m /workdir/exec/${BIN}
 #cp -r ${BIN}/exposureFusion /workdir/exec/
 #cp ${BIN}/robustNormalization.m  /workdir/exec/
-cp -r ${BIN}/* .
+cp -r ${BIN}/* ${BIN}/ . 
 cd ${BIN} && octave -W -qf run_ef.m $PARAM_EF ${FLAMOD[@]}
 
 #cp ${BIN}/*.png /workdir/exec/
